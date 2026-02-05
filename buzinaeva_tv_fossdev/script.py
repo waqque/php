27855478ea3bcf0249b2683abd092ce5438c4e32
waqque <tanos_hanos@mail.ru>
2026-02-05 14:54:23 +0800

    Added checking for string

--- buzinaeva_tv_fossdev/script.py
+++ buzinaeva_tv_fossdev/script.py
@@ -2,7 +2,7 @@ def sum(a, b):
     return a+b
 def divide(a, b):
     if b == 0:
-	raise ValueError("Division by zero is forbidden")
+	raise ValueError("Division by zero(0) is forbidden")
     if isinstance(a, str) or isinstance(b, str):
        	raise ValueError("Could not divide strings")
     return a/b
