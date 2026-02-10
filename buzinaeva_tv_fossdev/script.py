8b16cf31e6c7d97f10ac81c212bb572611d0ff01
waqque <tanos_hanos@mail.ru>
2026-02-10 14:38:25 +0800

    Edited list

--- buzinaeva_tv_fossdev/script.py
+++ buzinaeva_tv_fossdev/script.py
@@ -1,9 +1,12 @@
 def sum(a, b):
-    return a+b
+    return a-b
 def divide(a, b):
     if b == 0:
 	raise ValueError("Division by zero(0) is forbidden")
     if isinstance(a, str) or isinstance(b, str):
        	raise ValueError("Could not divide strings")
+    if isinstance(a, list) or isinstance(b, list):
+        raise ValueError("Could not divide lists")
+
     return a/b
 
